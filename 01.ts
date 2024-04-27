c874e7d558092633cef3d284d7b4f8756af24d87
Muhammad-Zahid <156499487+zahidrahimoon@users.noreply.github.com>
2024-04-27 09:29:08 +0500

    Day02

---
+++ 01.ts
@@ -0,0 +1,10 @@
+//Create a function that takes two parameters (a number and a string) and returns a string. Add type annotations to the function parameters and return type.
+
+
+function Sum(a: number, b: string): string {
+    return a.toString() + b;
+}
+const result = Sum(10, "15");
+
+console.log(result);
+
